bfd1df3a8b8aae272ad584f41fca236a39fffad1
Henrique Souza <henrique.asouza@outlook.com>
2022-01-15 14:37:02 -0300

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on http://localhost:4000");
+    console.log(
+      `Continue where you stoped: ${process.env.MATERIAL_CONTINUE ?? "unknown"}`
+    );
+  });
+};
+
+main();
+
+console.log("Hello word");
---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,11 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class UserResolver {
+  //   @Mutation(() => String)
+  //   registrer(
+  //       @Arg()
+  //   ) {
+  //     return "Hello Word";
+  //   }
+}
